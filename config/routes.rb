125dcc4e5ea3780e7a8361d9cab11a6c3356e33d
Sabina <sabina_stoicescu@yahoo.com>
2015-04-15 17:03:12 -0400

    Refactoring views to use helpers and adding navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'pictures#index'
   get 'pictures' => 'pictures#index'
   post 'pictures/create' => 'pictures#create'
   get 'pictures/new' => 'pictures#new'
